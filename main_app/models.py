30adcc0d7c614058fc7919e0c175b97a78c14bb5
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-03 16:32:29 -0400

    Adds recipe cards to recipes index

--- main_app/models.py
+++ main_app/models.py
@@ -3,8 +3,29 @@ from django.db import models
 # Create your models here.
 class Recipe(models.Model):
     name = models.CharField(max_length=150)
-    link = models.URLField(max_length=300)
-    image = models.ImageField()
+
+    APPETIZER = 'appetizer'
+    ENTREE = 'entree'
+    SIDE = 'side'
+    SAUCE = 'sauce'
+    DESSERT = 'dessert'
+    DRINK = 'drink'
+    RECIPE_CATEGORY_CHOICES = [
+        (APPETIZER, 'Appetizer'),
+        (ENTREE, 'Entree'),
+        (SIDE, 'Side Dish'),
+        (SAUCE, 'Sauce'),
+        (DESSERT, 'Dessert'),
+        (DRINK, 'Drink'),
+    ]
+
+    category = models.CharField(
+        max_length=10,
+        choices=RECIPE_CATEGORY_CHOICES,
+        default=ENTREE,
+    )
+    link = models.URLField(max_length=500)
+    image = models.URLField(max_length=500)
 
     def __str__(self):
-        return.self.name
+        return self.name
