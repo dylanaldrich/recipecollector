918088bbb783b4f4cfc3e8fb2cb8fd7f22f0bad6
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-03 14:41:32 -0400

    Adds several routes to app views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Recipe Collector Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About Recipe Collector</h1>')
+
+def recipes_index(request):
+    return HttpResponse('<h1>Recipes Index</h1>')
+
+def recipe_new(request):
+    return HttpResponse('<h1>New Recipe</h1>')
+
+def recipe_detail(request):
+    return HttpResponse('<h1>Recipe Details</h1>')
+
+def recipe_edit(request):
+    return HttpResponse('<h1>Edit a Recipe</h1>')
