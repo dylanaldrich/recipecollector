37666eee40c07ead76f325655c5f5989f96bfb48
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-03 15:52:33 -0400

    adds recipes model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=150)
+    link = models.URLField(max_length=300)
+    image = models.ImageField()
+
+    def __str__(self):
+        return.self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import Recipe
 
 # Create your views here.
 def home(request):
@@ -9,13 +10,19 @@ def about(request):
     return render(request, 'about.html')
 
 def recipes_index(request):
-    return render(request, 'recipes/index.html')
+    recipes = Recipe.objects.all()
+    context = {'recipes':recipes}
+    return render(request, 'recipes/index.html', context)
 
 def recipe_new(request):
     return render(request, 'recipes/new.html')
 
 def recipe_detail(request):
+    recipe = Recipe.objects.get(id=recipe_id)
+    context = {'recipe':recipe}
     return render(request, 'recipes/detail.html')
 
 def recipe_edit(request):
-    return HttpResponse('<h1>Edit a Recipe</h1>')
+    recipe = Recipe.objects.get(id=recipe_id)
+    context = {'recipe':recipe}
+    return render(request, 'recipes/edit.html', context)
