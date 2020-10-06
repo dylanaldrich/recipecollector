f5d34e5d2a4f34b5aa2bf08fe0ce0140efbc2b82
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-05 22:47:07 -0400

    adds many routes for all models, chapters and recipes routes still incomplete

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,13 +9,19 @@ urlpatterns = [
     path('books/', views.books_index, name='books_index'),
     path('books/<int:book_id>/', views.book_detail, name='book_detail'), # this lists chapters of a given book
     path('books/<int:book_id>/edit/', views.book_edit, name='book_edit'),
+    path('books/<int:book_id>/delete/', views.book_delete, name='book_delete'),
 
     # Chapter URLs 
     path('chapters/<int:chapter_id>/', views.chapter_detail, name='chapter_detail'), # this lists recipes of given chapter
+    path('chapters/<int:chapter_id>/add/', views.chapter_add, name='chapter_add'),
     path('chapters/<int:chapter_id>/edit/', views.chapter_edit, name='chapter_edit'),
+    path('chapters/<int:chapter_id>/delete/', views.chapter_delete, name='chapter_delete'),
+    
 
     # Recipes URLs
     path('recipes/', views.recipes_index, name='recipes_index'), # this lists all recipes of a given user
     path('recipes/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+    path('recipes/<int:recipe_id>/add/', views.recipe_add, name='recipe_add'),
     path('recipes/<int:recipe_id>/edit/', views.recipe_edit, name='recipe_edit'),
+    path('recipes/<int:recipe_id>/delete/', views.recipe_delete, name='recipe_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Book, Chapter, Recipe
+from .forms import Book_Form, Chapter_Form, Recipe_Form
+
 
 # Create your views here.
 def home(request): # this will become login page
@@ -10,36 +12,77 @@ def about(request):
     return render(request, 'about.html')
 
 
-# Books routes
+# SECTION Books routes
+# index && create
 def books_index(request):
+    if request.method == 'POST':
+        book_form = Book_Form(request.POST)
+        if book_form.is_valid():
+            book_form.save()
+            return redirect('books_index')
     books = Book.objects.all()
+    book_form = Book_Form()
     context = {'books':books}
     return render(request, 'books/index.html', context)
 
+# show
 def book_detail(request, book_id): # this serves as chapters index of a given book
     book = Book.objects.get(id=book_id)
-    context = {'book':book}
+    chapter_form = Chapter_Form()
+    context = {
+        'book':book,
+        'chapter_form':chapter_form,
+    }
     return render(request, 'books/detail.html', context)
 
+# edit && update
 def book_edit(request, book_id):
     book = Book.objects.get(id=book_id)
-    context = {'book':book}
+    if request.method == 'POST':
+        book_form = Book_Form(request.POST, instance=book)
+        if book_form.is_valid():
+            book_form.save()
+            return redirect('detail', book_id=book_id)
+    else:
+        book_form = Book_Form(instance=book)
+    context = {'book':book, 'book_form':book_form}
     return render(request, 'books/edit.html', context)
 
+# delete
+def book_delete(request, book_id):
+    Book.objects.get(id=book_id).delete()
+    return redirect('books_index')
 
-# Chapters routes
+
+# SECTION Chapters routes
+# show
 def chapter_detail(request, chapter_id): #this serves as recipes index of a given chapter
     chapter = Chapter.objects.get(id=chapter_id)
     context = {'chapter':chapter}
     return render(request, 'chapters/detail.html', context)
 
+# create
+def chapter_add(request, book_id):
+    chapter_form = Chapter_Form(request.POST)
+    if chapter_form.is_valid():
+        new_chapter = feeding_form.save(commit=False)
+        new_chapter.book_id = book_id
+        new_chapter.save()
+    return redirect('detail', book_id=book_id)
+
+# edit && update
 def chapter_edit(request, chapter_id):
     chapter = Chapter.objects.get(id=chapter_id)
     context = {'chapter':chapter}
     return render(request, 'chapters/edit.html', context)
 
+# delete
+def chapter_delete(request, chapter_id):
+    Chapter.objects.get(id=chapter_id).delete()
+    # TODO get this to redirect back to the parent book's detail page
+    return redirect('chapters_index')
 
-# Recipes routes
+# SECTION Recipes routes
 def recipes_index(request): # this lists ALL recipes of current user
     recipes = Recipe.objects.all()
     context = {'recipes':recipes}
@@ -54,3 +97,11 @@ def recipe_edit(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
     context = {'recipe':recipe}
     return render(request, 'recipes/edit.html', context)
+
+def chapter_add(request, book_id):
+    chapter_form = Chapter_Form(request.POST)
+    if chapter_form.is_valid():
+        new_chapter = feeding_form.save(commit=False)
+        new_chapter.book_id = book_id
+        new_chapter.save()
+    return redirect('detail', book_id=book_id)
