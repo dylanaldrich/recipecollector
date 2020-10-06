91f1b8acf07eab9405fb6c27e3037672c84c45c1
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-05 21:47:35 -0400

    Adds book, chapter and recipe form models

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,12 +1,28 @@
 from django.forms import ModelForm
-from .models import Recipe
+from .models import Book, Chapter, Recipe
+
+class Book_Form(ModelForm):
+    class Meta:
+        model = Book
+        fields = ['name']
+
+
+class Chapter_Form(ModelForm):
+    class Meta:
+        model = Chapter
+        fields = [
+            'name',
+            'description'
+        ]
+
 
 class Recipe_Form(ModelForm):
     class Meta:
         model = Recipe
         fields = [
             'name',
-            'category',
+            'source',
             'link',
             'image',
+            'notes',
         ]
