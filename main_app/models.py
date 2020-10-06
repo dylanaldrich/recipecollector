1e93a54d353d3fb43d9c29893b156afaffdd984c
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-06 18:52:45 -0400

    adds one to many and many to many associations between books, chapters, recipes

--- main_app/models.py
+++ main_app/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-class Book(models.Model):
-    name = models.CharField(max_length=40)
-
-    def __str__(self):
-        return self.name
-
-
-class Chapter(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=250)
-
-    def __str__(self):
-        return self.name
-
-
 class Recipe(models.Model):
     name = models.CharField(max_length=150)
     source = models.CharField(max_length=150)
@@ -26,3 +10,29 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        ordering = ['name']
+
+class Book(models.Model):
+    name = models.CharField(max_length=40)
+    recipes = models.ManyToManyField(Recipe)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Chapter(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    recipes = models.ManyToManyField(Recipe)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
